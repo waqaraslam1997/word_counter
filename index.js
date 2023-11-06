419ceb29715bcb5cbdc1a9770e52d166b786cf30
Waqar Aslam <waqar.king10@gmail.com>
2023-11-06 15:37:43 -0800

    CLI Word Counter

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.yellow("\n<<== ") + chalk.green("Welcome to Word Counter Application!") + chalk.yellow(" ==>>\n"));
+while (true) {
+    let { answer } = await inquirer.prompt({
+        type: "input",
+        name: "answer",
+        message: "Type & Hit Enter to count words!"
+    });
+    let result = answer.split(" ");
+    let word = "word";
+    if (result.length != 1) {
+        word = "words";
+    }
+    console.log(chalk.yellow.bold("\nYour wrote " + result.length + ` ${word}\n`));
+    const { again } = await inquirer.prompt({
+        type: "confirm",
+        message: "Count again? ",
+        default: true,
+        name: "again",
+    });
+    if (!again) {
+        console.log(chalk.bgBlueBright(chalk.white.bold(`\n== Stay Blessed & Good Bye! ==`)));
+        break;
+    }
+}
